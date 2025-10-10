f6bd94f1974bcf5b5392a5b2e892d29d4a9614ee
saniyajain05 <sjain14@depaul.edu>
2025-10-09 23:52:22 -0500

    Fix Vite root path and build config

--- 3D_portfolio_01/vite.config.js
+++ 3D_portfolio_01/vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss({config: '../tailwind.config.js',}),],
-  assetsInclude: ['**/*.glb']
-  
+    tailwindcss()
+  ],
+  assetsInclude: ['**/*.glb'],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
