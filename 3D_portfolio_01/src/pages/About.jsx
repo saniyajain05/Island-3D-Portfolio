02ba07fa052f2ef37ee812ec9ee1ea33064eac13
saniyajain05 <121827387+saniyajain05@users.noreply.github.com>
2026-02-27 11:50:28 -0600

    Update About.jsx

--- 3D_portfolio_01/src/pages/About.jsx
+++ 3D_portfolio_01/src/pages/About.jsx
@@ -7,7 +7,7 @@ import Footer from '../components/footer'
 
 import CTA from "../components/CTA";
 import { skills } from "../constants/socialLinks";
-import resumePDF from "../assets/Saniya_Resume_Master.pdf";
+import resumePDF from "../assets/Saniya_Resume_Areeg_2_20_2026.docx";
 
 
 import "react-vertical-timeline-component/style.min.css";
